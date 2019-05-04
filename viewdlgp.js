972c05faccc853dbbd27b62c84ecce2db714fd43
vladalazareva <vladalazareva@mail.ru>
2019-05-04 16:21:10 +0300

    Add files via upload

---
+++ viewdlgp.js
@@ -0,0 +1,6 @@
+function viewdlgp(){ 
+	document.getElementById("dlgp").style.display = "block";
+	document.getElementById("newdlgp").style.color = "blue";
+	document.getElementById("wrld").style.display = "none";
+	document.getElementById("newwrld").style.color = "powderblue";
+};
---
+++ viewwrld.js
@@ -0,0 +1,6 @@
+function viewwrld(){
+	document.getElementById("wrld").style.display = "block";
+	document.getElementById("newwrld").style.color = "blue";
+	document.getElementById("dlgp").style.display = "none";
+	document.getElementById("newdlgp").style.color = "powderblue";
+};
